d4af1880e3336512dd454508de30deb0996bc9a3
Dulara Lakshan <125289661+Nush987@users.noreply.github.com>
2024-11-03 12:51:52 +0530

    commit 01

---
+++ FactoryMethod/ShapeFactory.java
@@ -0,0 +1,14 @@
+package FactoryMethod;
+
+public class ShapeFactory {
+    public Shape getShape(String shapeType) {
+        if (shapeType == null) {
+            return null;
+
+        }
+        if (shapeType.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        }
+
+    }
+}
